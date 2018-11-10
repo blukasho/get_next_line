45ae51d6df5a4e104eaa4454b45f2fcc0a9c12d0
blukasho <bodik1w@gmail.com>
2018-11-10 16:00:17 +0200

    add libft

---
+++ libft/ft_memccpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blukasho <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/31 15:07:27 by blukasho          #+#    #+#             */
+/*   Updated: 2018/11/07 17:35:20 by blukasho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *restrict dst,
+					const void *restrict src, int c, size_t n)
+{
+	unsigned char	*ds;
+	unsigned char	*sr;
+	size_t			i;
+	unsigned char	ch;
+
+	ds = (unsigned char *)dst;
+	sr = (unsigned char *)src;
+	i = -1;
+	ch = (unsigned char)c;
+	while (++i < n)
+	{
+		ds[i] = sr[i];
+		if (ch == sr[i])
+			return (dst + (++i));
+	}
+	return (NULL);
+}
