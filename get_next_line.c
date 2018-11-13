a9e0f7177c067a439872708d7d3d9c56c742746c
blukasho <bodik1w@gmail.com>
2018-11-13 18:46:12 +0200

    mod get_next_line.c, mod get_next_line.h, mod libft/Makefile, mod libft/ft_strjoin.c, mod main.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,15 @@
 /*   By: blukasho <bodik1w@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/10 16:13:43 by blukasho          #+#    #+#             */
-/*   Updated: 2018/11/12 19:22:39 by blukasho         ###   ########.fr       */
+/*   Updated: 2018/11/13 18:43:28 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
+#include <stdio.h>
+/*
+ * del stdio.h
+ */
 char			*read_line(int fd, int *val, char *res)
 {
 	char		buf[BUFF_SIZE + 1];
@@ -19,25 +22,26 @@ char			*read_line(int fd, int *val, char *res)
 
 	tmp = res;
 	*val = read(fd, buf, BUFF_SIZE);
-	if (!(res = ft_strjoin(res, buf))
-			return (NULL);
+	buf[*val] = '\0';
+	if (!(res = ft_strjoin(res, buf)))
+		return (NULL);
 	ft_strdel(&tmp);
 	return (res);
 }
 
 int				get_next_line(const int fd, char **line)
 {
-	static char *buf;
+	static char	*buf;
 	int			val;
 
 	val = 1;
-	if (fd > 0 && line)
+	if (fd > 0 && val && (buf = ft_strnew(0)))
 	{
 		while (val > 0)
-		{
 			buf = read_line(fd, &val, buf);
-		}
-		return (0);
+		if (buf)
+
+		*line = ft_strdup(buf);
 	}
-	return (-1);
+	return (0);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: blukasho <bodik1w@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/10 18:13:49 by blukasho          #+#    #+#             */
-/*   Updated: 2018/11/12 16:59:35 by blukasho         ###   ########.fr       */
+/*   Updated: 2018/11/13 18:35:29 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,40 +20,11 @@ int			main(int argc, char **argv)
 	int		fd;
 	int		val;
 	char	*res;
+	char	*res2;
 
-	if (argc == 1)
-	{
-		ft_putstr("No filename!\n");
-		return (0);
-	}
-	else if (argc > 2)	
-	{
-		ft_putstr("To many arguments!\n");
-		return (0);
-	}
-	else if (argc == 2)
-	{
-		ft_putstr("Read file... \"");
-		ft_putstr(argv[1]);
-		ft_putstr("\"\n");
-		fd = open(argv[1], O_RDONLY);
-		if (fd < 0)
-		{
-			ft_putstr("Cant open file...\n");
-			return (0);
-		}
-		else
-		{
-			if ((val = get_next_line(fd, &res)))
-			{
-				ft_putstr(res);
-				ft_putchar('\n');
-			}
-		}
-	}
-	if (0 > close(fd))
-		ft_putstr("Cant close file.\n");
-	else
-		ft_putstr("File close successfully.\n");
+	fd = open(argv[1], O_RDONLY);
+	get_next_line(fd, &res);
+	ft_putstr(res);
+	ft_putchar('\n');
 	return (0);
 }
